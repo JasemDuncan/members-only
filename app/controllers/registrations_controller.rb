101f6f3604a52f6bc3f46e94741f96304d7f1c67
alexoid <alex_zam@hotmail.es>
2020-09-16 16:13:27 -0500

    add fields to usuarios

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,11 @@
+class RegistrationsController < Devise::RegistrationsController
+
+    private
+    def sign_up_paramns
+            params.require(:usuario).permit(:name, :username, :email, :password, :password_confirmation)
+    end
+
+    def account_update_paramns
+        params.require(:usuario).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
+    end
+end
