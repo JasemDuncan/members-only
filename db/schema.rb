db20d3b15544253b7e704dfa9f7e871978aef8f1
JasemDuncan <jasemvalencia@gmail.com>
2020-09-17 12:34:00 -0500

    Create session logic to signIn or SignUp or LogOut

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2020_09_17_152128) do
     t.text "post"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.integer "usuario_id"
   end
 
