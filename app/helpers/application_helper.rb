f7341a975e7183c9411ea1a56059b449b3b22457
JasemDuncan <jasemvalencia@gmail.com>
2020-09-18 17:58:49 -0500

    Fix rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,24 @@
 module ApplicationHelper
-    def registration_and_session_links
-        if signed_in?
-          link_to 'Logout', destroy_usuario_session_path, method: :delete,
-                                                       data: { confimation: 'are you sure?' }, class: 'nav-link text-dark'
-        else
-          link_to 'Sign up', new_usuario_registration_path, class: 'nav-link text-dark'
-    
-        end
-    end
-    
-    def signed_in_link
-        link_to 'Log in', new_usuario_session_path, class: 'nav-link text-dark' 
-    end
+  def registration_and_session_links
+    if signed_in?
+      link_to 'Logout', destroy_usuario_session_path, method: :delete,
+                                                      data: { confimation: 'are you sure?' },
+                                                      class: 'nav-link text-dark'
+    else
+      link_to 'Sign up', new_usuario_registration_path, class: 'nav-link text-dark'
 
-    def error_flashes
-        if flash[:notice]
-          content_tag(:p, flash[:notice], class: 'alert alert-success')
-        elsif flash[:alert]
-          content_tag(:p,  flash[:alert], class: 'alert alert-danger')
-        end
     end
+  end
+
+  def signed_in_link
+    link_to 'Log in', new_usuario_session_path, class: 'nav-link text-dark'
+  end
+
+  def error_flashes
+    if flash[:notice]
+      content_tag(:p, flash[:notice], class: 'alert alert-success')
+    elsif flash[:alert]
+      content_tag(:p, flash[:alert], class: 'alert alert-danger')
+    end
+  end
 end
