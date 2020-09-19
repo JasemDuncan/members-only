1e8c402ff62c1af15b5de98c2df23f6ba3d96d58
alexoid <alex_zam@hotmail.es>
2020-09-18 20:51:26 -0500

    add show name method helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,9 @@
 module PostsHelper
   def image_helper
-    content_tag(:div, content_tag(:div, content_tag(:p, content_tag(image_tag('https://bulma.io/images/placeholders/64x64.png', alt: 'Continue', class: 'img-container')), class: 'image is-64x64')), class: 'col-2 mx-auto bg-light') if signed_in?
+    content_tag(:p, content_tag(image_tag('https://bulma.io/images/placeholders/64x64.png', class: 'img-container')), class: 'image is-64x64') if signed_in?
+  end
+
+  def show_name(post)
+    content_tag(:p, post.usuario.name, class: 'text-center') if signed_in?
   end
 end
