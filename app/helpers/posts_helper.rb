4bd7644c1e1875d55306a50d526f7378d8085557
alexoid <alex_zam@hotmail.es>
2020-09-18 20:00:33 -0500

    add img helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,5 @@
 module PostsHelper
+  def image_helper
+    content_tag(:div, content_tag(:div, content_tag(:p, content_tag(image_tag('https://bulma.io/images/placeholders/64x64.png', alt: 'Continue', class: 'img-container')), class: 'image is-64x64')), class: 'col-2 mx-auto bg-light') if signed_in?
+  end
 end
